0b5fa36d42d3f46b8320280bf9c22ebccbe10f03
klaudia <klaudia.styrcz@gmail.com>
2019-03-08 23:01:20 +0100

    operators ternary and logical

---
+++ exercises/operators/operators_ternary+logical.js
@@ -0,0 +1,19 @@
+        var c = false;
+        var d = true;
+        var e = !(c && d) ? "This was true" : "was False :(";
+        console.log(e);
+
+        var a = 5;
+        var b = a + 10;
+        var output = document.getElementById("output");
+        var btn1 = document.getElementById('btn1');
+        var btn2 = document.getElementById('btn2');
+
+        btn1.onclick = function () {
+            a++;
+            output.innerHTML = b + a;
+        }
+        btn2.onclick = function () {
+            a--;
+            output.innerHTML = b + a;
+        }
